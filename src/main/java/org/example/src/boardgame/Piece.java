105de1a476a76e4da23a70d1c76eb784a9fb4902
oricardoz <ricardoapzaghetto@gmail.com>
2024-02-15 20:50:34 -0300

    start implement board and piece

---
+++ src/main/java/org/example/src/boardgame/Piece.java
@@ -0,0 +1,15 @@
+package org.example.src.boardgame;
+
+public class Piece {
+
+    protected Position position;
+    private Board board;
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+    protected Board getBoard() {
+        return board;
+    }
+
+}
