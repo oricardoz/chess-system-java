ca6ecfcf3fb5dfbb65d1fc1a3a075c34efecb4e6
oricardoz <ricardoapzaghetto@gmail.com>
2024-02-15 15:53:10 -0300

    first class position

---
+++ src/main/java/org/example/src/application/Program.java
@@ -0,0 +1,11 @@
+package org.example.src.application;
+
+import org.example.src.boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
